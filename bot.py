544cf560b9aaad14a86439ef2569028f54a173e2
Danny <51312422+dchenz@users.noreply.github.com>
2023-12-26 05:25:07 +0000

    Load bot configuration from yaml file

--- bot.py
+++ bot.py
@@ -1,42 +1,21 @@
+import yaml
+from schema import Schema
 from bot import LimelightBot
 
 
-if __name__ == "__main__":
-    import json
-    import os
-
-    import dotenv
-
-    def load_discord_ids_config(filename: str) -> list[int]:
-        loaded = []
-        try:
-            with open(filename, "r") as f:
-                ids = json.load(f)
-                loaded.extend(int(x) for x in ids)
-        except FileNotFoundError:
-            print("Could not load " + filename)
-            exit(1)
-        except json.JSONDecodeError:
-            pass
-        except ValueError:
-            print("Invalid ID in " + filename)
-            exit(1)
-        print(f"Loaded {len(loaded)} from {filename}")
-        return loaded
-
-    # Users who can run bot commands
-    allowed_users = load_discord_ids_config("config/allowed_users.json")
-
-    # Channels ignored by sync jobs
-    # Bot can still listen for commands here
-    ignored_channels = load_discord_ids_config("config/ignored_channels.json")
-
-    dotenv.load_dotenv()
-    bot = LimelightBot(
-        "&lime", allowed_users=allowed_users, ignored_channels=ignored_channels
+def load_config():
+    config_schema = Schema(
+        {"token": str, "allowed_users": [int], "ignored_channels": [int]}
     )
-    token = os.environ.get("TOKEN")
-    if not token:
-        print("Missing bot token")
-        exit(1)
-    bot.run(token)
+    with open("./config.yaml") as f:
+        return config_schema.validate(yaml.safe_load(f))
+
+
+if __name__ == "__main__":
+    config = load_config()
+    bot = LimelightBot(
+        "&lime",
+        allowed_users=config["allowed_users"],
+        ignored_channels=config["ignored_channels"],
+    )
+    bot.run(config["token"])
